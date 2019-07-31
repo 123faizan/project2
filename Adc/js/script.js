b40af968f0f7bb5a66cf92d9a943fc3fda5a3cb7
123faizan <53457934+123faizan@users.noreply.github.com>
2019-07-31 09:57:36 +0530

    initial commit

---
+++ Adc/js/script.js
@@ -0,0 +1,15 @@
+function validate(){
+document.getElementById("para").innerHTML="HTML stands for Hyper Text Markup LanguageHTML describes the structure of a Web page HTML consists of a series of elements HTML elements tell the browser how to display the content HTML elements are represented by tags ";
+}
+function validate1(){
+document.getElementById("para1").innerHTML="CSS stands for Cascading Style Sheets"+
+"\nCSS describes how HTML elements are to be displayed on screen, paper, or in other media"+
+"\nCSS saves a lot of work.\nIt can control the layout of multiple web pages all at once"+
+"External stylesheets are stored in CSS files";
+}
+function validate2(){
+document.getElementById("para2").innerHTML="When new users visit your website or product you should demonstrate your product features using a step-by-step guide. Even when you develop and add a new feature to your product, you should be able to represent them to your users using a user-friendly solution. Intro.js is developed to enable web and mobile developers to create a step-by-step introduction easily.";
+}
+function validate3(){
+document.getElementById("para3").innerHTML="User interface design is the design of user interfaces for different software or machines with a clear purpose: to make a better experience for users when navigating through your platform. The main focus is to clarify the usability of various tools and also create a great aspect";
+}
